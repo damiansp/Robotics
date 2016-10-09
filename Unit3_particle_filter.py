6eec7b47b7f77a9dbc5c396daaa9757124cd6ec8
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-10-08 21:45:46 -0400

    removed particleFilter4.py, moved content into Unit3_particle_filter.py

--- Unit3_particle_filter.py
+++ Unit3_particle_filter.py
@@ -43,56 +43,60 @@ print (myrobot.sense())
 # Use a particle filter-----------------------------------------------
 # Initialize 1000 particles
 N = 5000
-p = []
+T = 10 # Time steps
 
-
-# Intialize the robot move it and sense
+# Intialize the robot
 myrobot = robot()
-myrobot = myrobot.move(0.1, 5.0)
-Z = myrobot.sense()
 
 # Initialize particles
+p = []
 for i in range(N):
     x = robot()
     x.set_noise(0.05, 0.05, 5.0)
     p.append(x)
 
+for t in range(10):
+    myrobot = myrobot.move(0.1, 5.0)
+    Z = myrobot.sense()
 
-# Simulate robot's motion for all particles-- 
-for particle in range(N):
-    p[particle] = p[particle].move(0.1, 5)
+    # Simulate robot's motion for all particles-- 
+    for particle in range(N):
+        p[particle] = p[particle].move(0.1, 5)
 
     
-# Set weights to each particle
-w = []
-for particle in range(N):
-    w.append(p[particle].measurement_prob(Z))
+    # Set weights to each particle
+    w = []
+    for particle in range(N):
+        w.append(p[particle].measurement_prob(Z))
 
-# Resample weights according to probability
-# By hand:
-#p_new = []
-#index = int(random.uniform(0, N))
-#beta = 0
+    # Resample weights according to probability
+    # Using my own code::
+    #p_new = []
+    #index = int(random.uniform(0, N))
+    #beta = 0
 
-#for i in range(N):
-#    beta += random.uniform(0, 2 * max(w))
+    #for i in range(N):
+    #    beta += random.uniform(0, 2 * max(w))
 
-#    while w[index] < beta:
-#        beta -= w[index]
-#        index = (index + 1) % N
+    #    while w[index] < beta:
+    #        beta -= w[index]
+    #        index = (index + 1) % N
 
-#    p_new.append(p[index])
+    #    p_new.append(p[index])
 
-#p = p_new
+    #p = p_new
 
-# Using numpy:
-w = np.array(w)
-p = np.random.choice(p, len(p), replace = True, p = w / sum(w))
+    # Using numpy:
+    w = np.array(w)
+    p = np.random.choice(p, N, replace = True, p = w / sum(w))
 
-print ('Robot:', myrobot.__repr__())
-# Verify visually
-x = [a.x for a in p]
-y = [a.y for a in p]
+    print ('Robot:', myrobot.__repr__())
+    # Verify visually
+    x = [a.x for a in p]
+    y = [a.y for a in p]
 
-plt.plot(x, y, 'bo', alpha = 0.02)
-plt.show()
+    plt.plot(x, y, 'bo', alpha = 0.02)
+    plt.show()
+
+
+print p[:5]  # note orientations also converging
