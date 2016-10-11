63b186828c30b4ea1b168085487ca6ee9990875c
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-10-11 17:37:03 -0400

    added new noise vars and setters; made out-of-bounds optional; added noise to sense(); moved measurement_prob() to measurement_prob1() and added new measurement_prob() that usese the sense_bearing() measurements

--- robot.py
+++ robot.py
@@ -15,34 +15,46 @@ world_size = 100.0
 
 
 class robot:
-    def __init__(self, length = 10.0):
+    def __init__(self, length = 20.0, bounded = False):
         self.x = random.random() * world_size # initial x coord
         self.y = random.random() * world_size # initial y coord
         self.orientation = random.random() * 2.0 * pi # initial heading
         self.length = length                          # length of robot
-        self.forward_noise = 0.0;                     # error terms
-        self.turn_noise    = 0.0;                     # ...
-        self.sense_noise   = 0.0;                     # ...
+        self.forward_noise  = 0.0                     # error terms
+        self.turn_noise     = 0.0                     # ...
+        self.sense_noise    = 0.0                     # ...
+        self.bearing_noise  = 0.0
+        self.steering_noise = 0.0
+        self.distance_noise = 0.0
     
     def set(self, new_x, new_y, new_orientation):
-        if new_x < 0 or new_x >= world_size:
-            raise ValueError, 'X coordinate out of bound'
-        if new_y < 0 or new_y >= world_size:
-            raise ValueError, 'Y coordinate out of bound'
+        if bounded:
+            if new_x < 0 or new_x >= world_size:
+                raise ValueError, 'X coordinate out of bounds'
+            if new_y < 0 or new_y >= world_size:
+                raise ValueError, 'Y coordinate out of bounds'
         if new_orientation < 0 or new_orientation >= 2 * pi:
-            raise ValueError, 'Orientation must be in [0..2pi]'
+            raise ValueError, 'Orientation must be in [0, 2pi]'
         self.x = float(new_x)
         self.y = float(new_y)
         self.orientation = float(new_orientation)
     
     
-    def set_noise(self, new_f_noise, new_t_noise, new_s_noise):
+    def set_noise(self,
+                  new_b_noise,
+                  new_sn_noise,
+                  new_d_noise,
+                  new_f_noise,
+                  new_t_noise,
+                  new_st_noise):
         # makes it possible to change the noise parameters
         # this is often useful in particle filters
-        self.forward_noise = float(new_f_noise);
-        self.turn_noise    = float(new_t_noise);
-        self.sense_noise   = float(new_s_noise);
-    
+        self.forward_noise  = float(new_f_noise)
+        self.turn_noise     = float(new_t_noise)
+        self.sense_noise    = float(new_sn_noise)
+        self.bearing_noise  = float(new_b_noise)
+        self.steering_noise = float(new_st_noise)
+        self.distance_noise = float(new_d_noise)
     
     def sense(self):
         '''
@@ -112,6 +124,10 @@ class robot:
         @return: a robot object with new [x, y, heading]
         '''
         alpha, d = motion
+        # Add noise
+        alpha += random.gauss(0., self.steering_noise)
+        d += random.gauss(0, self.distance_noise)
+        
         # turning angle:
         beta = (d / self.length) * tan(alpha)
 
@@ -141,17 +157,14 @@ class robot:
         return result
             
     def Gaussian(self, mu, sigma, x):
-        
         # calculates the probability of x for 1-dim Gaussian with mean mu and
         # var. sigma
         return (exp(-((mu - x) ** 2) / (sigma ** 2) / 2.0) /
                 sqrt(2.0 * pi * (sigma ** 2)))
     
     
-    def measurement_prob(self, measurement):
-        
+    def measurement_prob1(self, measurement):
         # calculates how likely a measurement should be
-        
         prob = 1.0;
         for i in range(len(landmarks)):
             dist = (sqrt((self.x - landmarks[i][0]) ** 2 +
@@ -160,7 +173,21 @@ class robot:
         return prob
     
     
+    def measurement_prob(self, measurements):
+        predicted_measurements = self.sense(0)
+
+        # compute errors
+        error = 1.0
+        for i in range(len(measurements)):
+            error_bearing = abs(measurements[i] - predicted_measurements[i])
+            error_bearing = (error_bearing + pi) % (2.0 * pi) - pi
+            errror *= (exp(-(error_bearing ** 2) / (self.bearing_noise **2) /
+                           2.0) /
+                       sqrt(2.0 * pi * (self.bearing_noise ** 2)))
+
+        return error
     
+        
     def __repr__(self):
         return ('[x=%.6s y=%.6s orient=%.6s]'
                 % (str(self.x), str(self.y), str(self.orientation)))
