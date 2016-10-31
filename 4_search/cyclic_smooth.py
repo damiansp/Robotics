7d1d4f93529034bbd0f666e6694bdce3f2ac109e
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2016-10-31 15:57:26 -0400

    smoothing when path is a circuit

---
+++ 4_search/cyclic_smooth.py
@@ -0,0 +1,77 @@
+# -------------
+# User Instructions
+#
+# Here you will be implementing a cyclic smoothing
+# algorithm. This algorithm should not fix the end
+# points (as you did in the unit quizzes). You
+# should use the gradient descent equations that
+# you used previously.
+#
+# Your function should return the newpath that it
+# calculates.
+#
+# Feel free to use the provided solution_check function
+# to test your code. You can find it at the bottom.
+#
+# --------------
+# Testing Instructions
+#
+# To test your code, call the solution_check function with
+# two arguments. The first argument should be the result of your
+# smooth function. The second should be the corresponding answer.
+# For example, calling
+#
+# solution_check(smooth(testpath1), answer1)
+#
+# should return True if your answer is correct and False if
+# it is not.
+
+from math import *
+from copy import deepcopy
+
+# Do not modify path inside your function.
+path = [[0, 0],
+        [1, 0],
+        [2, 0],
+        [3, 0],
+        [4, 0],
+        [5, 0],
+        [6, 0],
+        [6, 1],
+        [6, 2],
+        [6, 3],
+        [5, 3],
+        [4, 3],
+        [2, 3],
+        [1, 3],
+        [0, 3],
+        [0, 2],
+        [0, 1]]
+
+def smooth(path, weight_data = 0.1, weight_smooth = 0.1, tolerance = 0.00001):
+    newpath = deepcopy(path)
+    change = tolerance
+
+    while change >= tolerance:
+        change = 0.
+
+        for i in range(len(path)):
+            for j in range(len(path[0])):
+                aux = newpath[i][j]
+                newpath[i][j] += weight_data * (path[i][j] - aux)
+                newpath[i][j] += weight_smooth * (
+                    newpath[(i - 1) % len(path)][j] +
+                    newpath[(i + 1) % len(path)][j] -
+                    2. * newpath[i][j])
+            change += abs(aux - newpath[i][1])
+            
+    return newpath
+
+
+def printpaths(path, newpath):
+    for old, new in zip(path, newpath):
+        print ('['+ ', '.join('%.3f'%x for x in old) +
+               '] -> ['+ ', '.join('%.3f'%x for x in new) +']')
+        
+
+printpaths(path, smooth(path))
